60c107bea9d4e63df29e71fd7a0ba9b13f50922d
Manuel <17874544+MKoesters@users.noreply.github.com>
2020-03-27 14:57:17 +0100

    update param sweep script

--- scripts/01_offset_sweep.py
+++ scripts/01_offset_sweep.py
@@ -9,8 +9,23 @@ from scipy.stats import norm, normaltest
 import sys
 
 
-def draw_heatmap(tol_dict):
-    pass
+def draw_heatmap(plot_name, tol_dict, prec_tolerances, frag_tolerances):
+    z = np.zeros(
+        (len(prec_tolerances), len(frag_tolerances))
+    )
+    for i, prec_tol in enumerate(prec_tolerances):
+        for j, frag_tol in enumerate(frag_tolerances):
+            z[i, j] = tol_dict[(prec_tol, frag_tol)]
+    fig, ax = plt.subplots()
+    im = ax.imshow(z)
+    ax.set_xticklabels(['-'] + frag_tolerances)
+    ax.set_yticklabels(['-'] + prec_tolerances)
+    fig.colorbar(im)
+    plt.xlabel('frag tolerance [ppm]')
+    plt.ylabel('prec tolerance [ppm]')
+    plt.title(plot_name)
+    plt.savefig(plot_name)
+    return z
 
 
 @click.command()
@@ -18,13 +33,13 @@ def draw_heatmap(tol_dict):
 @click.argument('database')
 @click.option('--force', '-f', is_flag=True, help="Print more output.")
 def main(files, database, force):
-    engine = 'msgfplus_v2019_04_18'
+    # engine = 'msgfplus_v2019_04_18'
+    # engine = 'msfragger_20190222'
     engine = 'xtandem_alanine'
-    engine = 'msfragger_20190222'
     params = {
         # 'use_pyqms_for_mz_calculation': True,
         'database': database,
-        'scan_skip_modulo_step': 5,
+        'scan_skip_modulo_step': 2,
         'precursor_true_tolerance': 5,
         'precursor_mass_tolerance_unit': 'ppm',
         'precursor_mass_tolerance_plus': 300,
@@ -51,9 +66,14 @@ def main(files, database, force):
     uc = ursgal.UController(params=params, profile='QExactive+', verbose=False)
     means = []
 
-    precursor_tolerances = [20, 15, 10, 5, 2, 1]
-    frag_tolerances = [20, 15, 10, 5, 2, 1]
+    # in uncomment this, remember to change frag_tolerance unit!
+    # frag_tolerances_dalton = [50, 25, 17.5, 10, 5, 2.5]
+    precursor_tolerances = [20, 17.5, 15  , 12.5, 10  , 5 ]
+    frag_tolerances      = [25, 20  , 17.5, 15  , 12.5, 10]
+    all_matrices = []
     for file in files:
+        if '01' in file:
+            continue
         uc.params['machine_offset_in_ppm'] = 0
         uc.params['prefix'] = ''
         uc.params['precursor_mass_tolerance_unit'] = 'ppm'
@@ -64,30 +84,7 @@ def main(files, database, force):
         # 'precursor_mass_tolerance_minus': 5,
         # 'precursor_mass_tolerance_plus': 5,
 
-        # search = uc.search(file, engine=engine, force=True)
-
-        raw_search_results = uc.execute_misc_engine(
-            input_file = file,
-            engine     = engine,
-            force      = True,
-        )
-        csv_search_results = uc.convert(
-            input_file = raw_search_results,
-            engine = None,
-            guess_engine = True,
-            force      = force,
-        )
-        mapped_csv_search_results = uc.execute_misc_engine(
-            input_file       = csv_search_results,
-            engine           = uc.params['peptide_mapper_converter_version'],
-            force            = force,
-        )
-        search = uc.execute_misc_engine(
-            input_file       = mapped_csv_search_results,
-            engine           = self.params['unify_csv_converter_version'],
-            force            = force,
-            merge_duplicates = True,
-        )
+        search = uc.search(file, engine=engine, force=force)
 
         validated = uc.validate(search, engine='percolator_3_4', force=force)
         # validated = uc.validate(search, engine='percolator_2_08', force=force)
@@ -104,6 +101,8 @@ def main(files, database, force):
             uc.params['precursor_mass_tolerance_plus'] = prec_tol
             uc.params['precursor_mass_tolerance_minus'] = prec_tol
             for frag_tol in frag_tolerances:
+                # if prec tol > frag_tol:
+                #     continue
                 uc.params['prefix'] = f'corrected_mgf_{prec_tol}_{frag_tol}'
                 uc.params['frag_mass_tolerance'] = frag_tol
                 search = uc.search(corrected_mgf, engine=engine, force=force)
@@ -112,10 +111,18 @@ def main(files, database, force):
                 filtered = uc.execute_misc_engine(validated, engine='filter_csv_1_0_0', force=force)
                 results = pd.read_csv(filtered)
                 tolerance_dict[(prec_tol, frag_tol)] = len(results)
-                from pprint import pprint
-                pprint(tolerance_dict)
-
-        draw_heatmap(dict)
+                file_basename = os.path.splitext(os.path.basename(file))[0]
+                plot_name = f'{file_basename}_heatmap.png'
+        z = draw_heatmap(plot_name, tolerance_dict, precursor_tolerances, frag_tolerances)
+        all_matrices.append(z)
+    base_matrix = np.zeroes(
+        (
+            len(precursor_tolerances),
+            len(frag_tolerances),
+        )
+    )
+    for m in all_matrices:
+        pass
 
     # df = pd.DataFrame({'file': [os.path.basename(f) for f in files], 'offset': means})
     # df.to_csv('optimal_offsets.csv', index=False)
